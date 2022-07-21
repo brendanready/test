08f8c7ecbb5b9bfeaff5a6857e54a7a120c4a070
Brendan Ready <brendanbready@gmail.com>
2022-07-21 05:45:15 -0700

    Test Changes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("Page is working");
+
+let coinFlip;
+
+do {
+	coinFlip = Math.round(Math.random());
+	if (coinFlip === 0) {
+		console.log("Heads");
+		continue;
+	} else {
+		console.log("Tails");
+	}
+} while (coinFlip === 0);
